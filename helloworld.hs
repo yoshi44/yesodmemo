7ffcb39a2e0338a8ac4f7b8117c02f8cbafce0a6
yoshiVirtualBox <yoshivirtualbox@yoshiVirtualBoxs-MacBook-Pro.local>
2016-08-21 20:09:12 +0900

    [branch_name] add hello world

---
+++ helloworld.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import Yesod
+ 
+data HelloWorld = HelloWorld
+ 
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+ 
+instance Yesod HelloWorld
+ 
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+ 
+main :: IO ()
+main = warp 3000 HelloWorld
+
+-- how to run
+-- stack runghc helloworld.hs
+
